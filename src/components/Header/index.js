4aa1ab785e7a2c7cbcbed5e8a3499ce6fe3fb0a5
Rizwan Khan <rizwankhan.august16@gmail.com>
2020-10-25 10:06:15 -0700

    change

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
+import flipkartLogo from '../../images/logo/flipkart.png';
+import goldenStar from '../../images/logo/golden-star.png';
+import { IoIosArrowDown, IoIosCart, IoIosSearch } from 'react-icons/io';
+import { 
+  Modal,
+  MaterialInput,
+  MaterialButton,
+  DropdownMenu
+} from '../MaterialUI';
 
 /**
 * @author
@@ -7,10 +16,129 @@ import './style.css';
 **/
 
 const Header = (props) => {
-  return(
-    <div className="header"></div>
-   )
 
- }
+  const [loginModal, setLoginModal] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  
+
+  return (
+    <div className="header">
+      <Modal 
+        visible={loginModal}
+        onClose={() => setLoginModal(false)}
+      >
+        <div className="authContainer">
+          <div className="row">
+            <div className="leftspace">
+              <h2>Login</h2>
+              <p>Get access to your Orders, Wishlist and Recommendations</p>
+            </div>
+            <div className="rightspace">
+          
+
+                <MaterialInput 
+                  type="text"
+                  label="Enter Email/Enter Mobile Number"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+
+                <MaterialInput 
+                  type="password"
+                  label="Enter Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  rightElement={<a href="#">Forgot?</a>}
+                />
+                <MaterialButton 
+                  title="Login"
+                  bgColor="#fb641b"
+                  textColor="#ffffff"
+                />
+
+              
+
+            </div>
+          </div>
+        </div>
+      </Modal>
+      <div className="subHeader">
+        <div className="logo">
+          <a href="">
+            <img src={flipkartLogo} className="logoimage" alt="" />
+          </a>
+          <a style={{ marginTop: '-10px' }}>
+            <span className="exploreText">Explore</span>
+            <span className="plusText">Plus</span>
+            <img src={goldenStar} className="goldenStar" alt="" />
+          </a>
+        </div>
+        <div style={{
+          padding: '0 10px'
+        }}>
+          <div className="searchInputContainer">
+            <input
+              className="searchInput"
+              placeholder={'search for products, brands and more'}
+            />
+            <div className="searchIconContainer">
+              <IoIosSearch style={{
+                color: '#2874f0'
+              }} />
+            </div>
+
+          </div>
+        </div>
+        <div className="rightMenu">
+          <DropdownMenu
+            menu={
+              <a className="loginButton" onClick={() => setLoginModal(true)}>
+                Login
+              </a>
+            }
+            menus={[
+              { label: 'My Profile', href: '', icon: null },
+              { label: 'Flipkart Plus Zone', href: '', icon: null },
+              { label: 'Orders', href: '', icon: null },
+              { label: 'Wishlist', href: '', icon: null },
+              { label: 'Rewards', href: '', icon: null },
+              { label: 'Gift Cards', href: '', icon: null },
+            ]}
+            firstMenu={
+              <div className="firstmenu">
+                <span>New Customer?</span>
+                <a style={{ color: '#2874f0' }}>Sign Up</a>
+              </div>
+            }
+          />
+          <DropdownMenu
+            menu={
+              <a className="more">
+                <span>More</span>
+                <IoIosArrowDown />
+              </a>
+            }
+            menus={[
+              { label: 'Notification Preference', href: '', icon: null },
+              { label: 'Sell on flipkart', href: '', icon: null },
+              { label: '24x7 Customer Care', href: '', icon: null },
+              { label: 'Advertise', href: '', icon: null },
+              { label: 'Download App', href: '', icon: null }
+            ]}
+          />
+          <div>
+            <a className="cart">
+              <IoIosCart />
+              <span style={{ margin: '0 10px' }}>Cart</span>
+            </a>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  )
+
+}
 
 export default Header
---
+++ src/components/MaterialUI/index.js
@@ -0,0 +1,96 @@
+import React, { useState } from 'react';
+import './style.css';
+
+/**
+* @author Rizwan Khan
+* @function 
+**/
+
+const Modal = (props) => {
+    if (!props.visible) {
+        return null;
+    }
+    return (
+        <>
+            <div className="modalFixedBg">
+                <div style={{ position: 'relative' }}>
+                    <div className="modalClose" onClick={props.onClose}>X</div>
+                    <div className="modalContainer">
+                        {props.children}
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+const MaterialInput = (props) => {
+    const [focus, setFocus] = useState(false);
+
+    return (
+        <div className="materialInput">
+            <label className={`label ${focus ? 'focus' : ''}`} style={{
+                top: 0,
+                lineHeight: 'none'
+            }}>{props.label}</label>
+            <div style={{
+                display: 'flex'
+            }}>
+                <input className="input"
+                    type={props.type}
+                    value={props.value}
+                    onChange={props.onChange}
+                    onFocus={(e) => {
+                        setFocus(true)
+                    }}
+                    onBlur={(e) => {
+                        if(e.target.value === ""){
+                            setFocus(false)
+                        }
+                    }} />
+                {
+                    props.rightElement ? props.rightElement : null
+                }
+            </div>
+        </div>
+    )
+}
+
+const MaterialButton = (props) => {
+    return (
+        <div style={{ width: '90%' }}>
+            <button
+                className="materialButton"
+            >
+                {props.title && props.title}
+            </button>
+        </div>
+
+    )
+}
+
+const DropdownMenu = (props) => {
+    return (
+      <div className="headerDropdownContainer">
+        {props.menu}
+        <div className="dropdown">
+          <div className="upArrow"></div>
+          {props.firstMenu}
+          <ul className="headerDropdownMenu">
+            {
+              props.menus && props.menus.map((item, index) =>
+                <li key={index}><a href={item.href}>{item.label}</a></li>
+              )
+            }
+          </ul>
+        </div>
+      </div>
+    );
+  }
+
+export {
+    Modal,
+    MaterialInput,
+    MaterialButton,
+    DropdownMenu
+}
